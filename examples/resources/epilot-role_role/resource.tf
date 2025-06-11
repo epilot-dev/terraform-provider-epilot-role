732b0f390fafc7660b22221886604ea63a15b60b
Brendon Faccion <brendon.faccion@epilot.cloud>
2025-06-11 14:08:14 +0200

    chore: updating resources

--- examples/resources/epilot-role_role/resource.tf
+++ examples/resources/epilot-role_role/resource.tf
@@ -1,6 +1,24 @@
+terraform {
+  required_providers {
+    epilot-role = {
+      source  = "epilot-dev/epilot-role"
+      version = "0.16.1"
+    }
+  }
+}
+
+provider "epilot-role" {
+  # Configuration options
+  epilot_auth = var.epilot_auth
+}
+
+variable "epilot_auth" {
+  type = string
+}
+
 resource "epilot-role_role" "my_role" {
   schemas = {
-    expires_at = "2028-07-21T17:32:28Z"
+  #   expires_at = "2028-07-21T17:32:28Z"
     grants = [
       {
         action = "entity-read"
@@ -37,12 +55,12 @@ resource "epilot-role_role" "my_role" {
         resource = "entity:123:contact:f7c22299-ca72-4bca-8538-0a88eeefc947"
       }
     ]
-    id              = "123:owner"
-    name            = "Owner"
-    organization_id = "123"
-    partner_org_id  = "123"
-    pricing_tier    = "Professional"
-    slug            = "owner"
+    id              = "20000123:terraform-created-role"
+    name            = "Terraform Created Role"
+    organization_id = "20000123"
+    # partner_org_id  = "123"
+    # pricing_tier    = "Professional"
+    slug            = "terraform-created-role"
     type            = "user_role"
   }
 }
